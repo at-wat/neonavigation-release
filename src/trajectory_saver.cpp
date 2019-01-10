e43b288f142a94aea771db3d68c69ed869589edd
Atsushi Watanabe <atsushi.w@ieee.org>
2019-01-10 06:01:56 +0000

    Rebase from 'release/kinetic/trajectory_tracker'

--- src/trajectory_saver.cpp
+++ src/trajectory_saver.cpp
@@ -10,8 +10,8 @@
  *     * Redistributions in binary form must reproduce the above copyright
  *       notice, this list of conditions and the following disclaimer in the
  *       documentation and/or other materials provided with the distribution.
- *     * Neither the name of the copyright holder nor the names of its 
- *       contributors may be used to endorse or promote products derived from 
+ *     * Neither the name of the copyright holder nor the names of its
+ *       contributors may be used to endorse or promote products derived from
  *       this software without specific prior written permission.
  *
  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
@@ -28,8 +28,8 @@
  */
 
 /*
-   * This research was supported by a contract with the Ministry of Internal 
-   Affairs and Communications entitled, 'Novel and innovative R&D making use 
+   * This research was supported by a contract with the Ministry of Internal
+   Affairs and Communications entitled, 'Novel and innovative R&D making use
    of brain structures'
 
    This software was implemented to accomplish the above research.
@@ -39,8 +39,6 @@
 
 #include <geometry_msgs/Twist.h>
 #include <nav_msgs/Path.h>
-#include <tf/transform_listener.h>
-#include <tf/transform_datatypes.h>
 
 #include <math.h>
 #include <fstream>
@@ -59,7 +57,6 @@ private:
   ros::NodeHandle nh_;
   ros::NodeHandle pnh_;
   ros::Subscriber sub_path_;
-  tf::TransformListener tfl_;
 
   std::string topic_path_;
   std::string filename_;
